2204f16359ba9f56e98ff5aa304c18e89fd758d2
Anshul Katta <anshul.katta@gmail.com>
2023-04-22 13:12:12 +0100

    fix test add coverage run command in package

--- todo-ts-ui/src/App.tsx
+++ todo-ts-ui/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import TodoBrowser from './todo-browser/TodoBrowser'
+import TodoBrowser from 'src/todo-browser/TodoBrowser'
 
 function App() {
   return (
     <div className="App">
+      <p>Todo App</p>
       <header className="App-header">
         <TodoBrowser/>
       </header>
