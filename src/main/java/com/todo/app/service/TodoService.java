f21daef90dc9a4449cf2fbbbb2aaf0a2232e783c
Anshul Katta <anshul.katta@gmail.com>
2023-04-23 16:05:01 +0100

    Add get all todo list api Add unit tests

--- src/main/java/com/todo/app/service/TodoService.java
+++ src/main/java/com/todo/app/service/TodoService.java
@@ -3,6 +3,9 @@ package com.todo.app.service;
 import com.todo.app.model.TodoItem;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class TodoService {
 
@@ -13,5 +16,13 @@ public class TodoService {
     public TodoItem getTodoItemById(Long id) {
         // get by Id
         return new TodoItem();
+    }
+
+    public Iterable<TodoItem> getAllItems() {
+        List<TodoItem> todoItems = new ArrayList<>();
+        TodoItem todoItem = new TodoItem();
+        todoItem.setTitle("Todo 1");
+        todoItems.add(todoItem);
+        return todoItems;
     }
 }
--- src/main/java/com/todo/app/util/Constants.java
+++ src/main/java/com/todo/app/util/Constants.java
@@ -5,6 +5,9 @@ public class Constants {
     public static final String TODO_ENDPOINT = "/todo";
 
     public static final String CREATE_ITEM = "/createItem";
+    public static final String GET_BY_ID = "/{id}";
+
+    public static final String GET_ALL = "/getAllItems";
 
     public static final String SUCCESS = "SUCCESS";
     public static final String FAILED = "FAILED";
