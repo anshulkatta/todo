6f46b1ab522b1b57784ac2fab8c323609b7c6c1b
Anshul Katta <anshul.katta@gmail.com>
2023-04-23 18:24:54 +0100

    Add cypress testing framework Add ViewTodoItem.tsx and cypress test

---
+++ todo-ts-ui/src/tests/components/todo-item/ViewTodoItem.cy.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ViewTodoItem from 'src/components/todo-item/ViewTodoItem'
+import 'src/index.css'
+
+describe('<ViewTodoItem />', () => {
+  let todoItem = {
+    title: 'test',
+    id: 1,
+    completed: false,
+    description: 'test'
+  }
+
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<ViewTodoItem todoItem={todoItem} />)
+  })
+})
